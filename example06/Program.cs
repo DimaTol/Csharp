33d62925e1430c2e87c4617fa20346da0e7eaf1d
DimaTol <114100410+DimaTol@users.noreply.github.com>
2022-10-01 12:12:26 +0300

    Урок про гири

--- example06/Program.cs
+++ example06/Program.cs
@@ -1,2 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+int a = 1;
+int b = 2;
+int c = 3;
+int d = 5;
+int e = 6;
+int max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+if (d > max) max = d;
+if (e > max) max = e;
+Console.Write ("max = ");
+Console.WriteLine (max);
