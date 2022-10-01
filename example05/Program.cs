757336ea15c051079ead89a52084a2eefa0b6132
DimaTol <114100410+DimaTol@users.noreply.github.com>
2022-10-01 11:52:16 +0300

    Привет МаШа

--- example05/Program.cs
+++ example05/Program.cs
@@ -2,7 +2,7 @@
 Console.WriteLine("Введите имя пользователя");
 string username = Console.ReadLine();
 
-if (username == "Маша")
+if (username.ToLower() == "маша")
 {
     Console.WriteLine("Ура, это же Маша");
 }
